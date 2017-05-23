5123a93e6e6037875ea2779d8ef95eb3c7da8858
techmaster <techmaster@techmasters-iMac-5.local>
2017-05-23 11:02:38 +0700

    first commit

--- ZingMp3/ViewController.swift
+++ ZingMp3/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+
+    @IBOutlet weak var lbl_comback: UILabel!
+    @IBOutlet weak var lbl_text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        logo!.alpha = 0
+        lbl_text!.alpha = 0
+        lbl_comback!.alpha = 0
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIView.animate(withDuration: 4, animations: {
+            self.logo!.alpha = 1
+        }, completion: { (finished) in
+            UIView.animate(withDuration: 3, animations: {
+                self.lbl_text!.center = CGPoint(x: self.logo!.center.x, y: 100)
+                self.lbl_text!.alpha = 1
+           
+        }, completion:{ (finished) in
+            UIView.animate(withDuration: 3, animations: {
+                self.lbl_comback!.center = CGPoint(x: self.logo!.center.x, y: 600)
+                self.lbl_comback!.alpha = 1
+            })
+        })
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    })
 }
-
+}
